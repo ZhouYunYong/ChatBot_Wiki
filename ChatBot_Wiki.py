09436babe139cff1720cd8b5b9f4b5f7fb4050a7
MaxZhou <u9822113@gm.nuu.edu.tw>
2018-09-18 22:17:59 +0800

    version_2_add_wiki_speak

--- ChatBot_Wiki.py
+++ ChatBot_Wiki.py
@@ -7,6 +7,9 @@ import sys
 question = ''
 answer = ''
 QA = {'你好嗎': '我很好', '你是誰': '我是小美'}
+stt_language = 'zh-tw'
+wikiWord = '灌籃高手'
+# 特定名字與標題 《櫻桃小丸子》 《灌籃高手》 《道路交通法》 會有問題
 
 def stt():
     global question, answer
@@ -14,7 +17,7 @@ def stt():
         recong = speech_recognition.Recognizer()                        # 建立辨識物件
         with speech_recognition.Microphone(chunk_size = 2048) as voice: # 打開麥克風取得聲音
             audio = recong.listen(voice)                                # 讓辨識物件聽到的聲音
-        question = recong.recognize_google(audio, language='zh-TW')     # 將聲音資料翻成文字
+        question = recong.recognize_google(audio, language=stt_language)     # 將聲音資料翻成文字
 #        print(question)
         if question in QA.keys():
            answer = QA[question] 
@@ -34,7 +37,6 @@ import tempfile
 
 mixer.init()                                                # 建立 mixer 物件並初始化
 
-
 def speak(t, lang):
     try:
         with tempfile.NamedTemporaryFile(delete=True) as tf:    # 打開暫存檔
@@ -45,15 +47,54 @@ def speak(t, lang):
     except:
         print("Unexpected error:", sys.exc_info()[0])           # 無文字可以說
         
-if '__name__' == 'main':   
-    stt()
-    speak(answer, 'zh-tw')
+        
+ 
 
-# 翻譯蒟蒻
+if __name__ == '__main__':   
+#    print(stt())
+#    speak(answer, 'zh-tw')
+    pass
+
 
 # Wiki 爬蟲 
 
+import requests 
+from bs4 import BeautifulSoup 
+import re
 
+wikiWord = stt()
+print(wikiWord)
+response = requests.get('https://zh.wikipedia.org/wiki/{}'.format(wikiWord))
+
+if response.status_code == 200:
+#    print(response.text)                                       # 發出請求取得網頁文字內容
+    bs = BeautifulSoup(response.text, 'html.parser')            # 用 BeautifulSoup 解析原始內容
+    content = bs.select('.mw-parser-output p')                  # 回傳 list
+
+    for i in range(len(content)):                               # 在 list 中尋找 wikiWord 開頭的段落
+        if content[i].text[0 : len(wikiWord)] == wikiWord or content[i].text[1 : len(wikiWord)+1] == wikiWord :
+#            print(content[i].text)
+            re_text = re.sub(r'\[[0-9+]\]', '', content[i].text)
+            print(re_text)
+            speak(content[i].text, 'zh-tw')
+            break
+    
+#    speak(content, 'zh-tw')
+else:
+    print("查無資料")
+        
+
+
+
+#mixer.music.stop()
+    
+    
+
+    
+    
+    
+    
+    
 
 
 
--- Konjac.py
+++ Konjac.py
@@ -3,7 +3,7 @@ import ChatBot_Wiki
 # 安裝套件 pip install googletrans
 from googletrans import Translator
 
-lang = 'ja'   # zh-tw, ja
+trans_lang = 'ja'   # zh-tw, ja
 
 translator = Translator() # 建立翻譯物件
 
@@ -12,7 +12,7 @@ translator = Translator() # 建立翻譯物件
 
 speech = ChatBot_Wiki.stt()
 print(speech)
-result = translator.translate(speech, dest=lang).text
+result = translator.translate(speech, dest=trans_lang).text
 print(result)
-ChatBot_Wiki.speak(result, lang)   
+ChatBot_Wiki.speak(result, trans_lang)   
 
